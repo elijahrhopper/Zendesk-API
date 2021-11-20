9a3e6a9204544260cc7ca54ac5aeacfe80e40755
Elijah Hopper <elijahrhopper@gmail.com>
2021-11-19 21:27:10 -0500

    functionality to iterate through tickets

--- testAPIcontroller.py
+++ testAPIcontroller.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from requests import auth
 #import argparse
@@ -5,14 +6,20 @@ from requests import auth
 def main():
     print("Welcome to the Zendesk API coding assesment! Enter your credentials to begin:")
     email = str(input("Input your email: "))
-    authKey = "insert_auth_key_here"
-    if email == "Y":                    #TEMPORARY BYPASS FOR TESTING PURPOSES (TODO: DELETE)
-        email = "insert_email_here"
+    authKey = "auth_key_here"
+    if email == "Y": #TODO: DELETE THIS -- IT IS FOR TESTING PURPOSES ONLY
+        email = "email_here"
     else:
         authKey = str(input("Input your auth token: "))
 
     url = "https://zccelijah.zendesk.com/api/v2/tickets.json"
     response = requests.get(url, auth=(email+"/token", authKey))
+
+    
+
+    tickets = response.json()
+    for i in range(5):
+        print(str(i) + ": " + tickets["tickets"][i]["description"])
     
     if response:
         print('Response OK')
